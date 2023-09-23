d3b78f9a55c6b1381af8dc8f7f8fdabc3af6273c
Thiagocgomes <145858546+Thiagocgomes@users.noreply.github.com>
2023-09-23 17:19:21 -0300

    Add files via upload

---
+++ AULA_2_PYTHON.py
@@ -0,0 +1,36 @@
+# dinamica/ interpretada/ OOP/ alto nivel
+# convensões == boas práticas
+# nomear de forma semãnticas == dar o nome daquilo que o objeto faz
+# usar snake case
+# inglês = o código precis ser universal
+# crie um sistema que mostre o texto na tela
+
+# int
+# float
+# string
+
+# numero = 2 
+# numero2 = 20
+# numero3 = numero + numero2
+# print (numero3)
+
+
+# if numero3 == numero2:
+#  print("é igual")
+# else:
+#  print("é diferente")
+
+print("Calculadora de Soma")
+n1 = int(input("Digite o primeiro número:\n"))
+n2 = int(input("Digite o segundo número:\n"))
+n3 = n1 + n2
+print("O resultado da soma é: ",n3)
+
+nome = input("Digite o seu nome:\n")
+print("Seja Bem Vindo",nome)
+
+print("Calculadora de Multiplicação")
+n4 = int(input("Digite o primeiro número:\n"))
+n5 = int(input("Digite o segundo número:\n"))
+n6 = n4 * n5
+print("O resultado da multiplicação é: ",n6)
